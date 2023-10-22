0df77313ef98276625f6835e956311f72106d83a
Mdu Kelvin Letsie <mdu.letsie7@gmail.com>
2023-10-22 20:34:11 +0200

    Security updates

--- src/main/java/za/ac/cput/controller/HomeController.java
+++ src/main/java/za/ac/cput/controller/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String getOpen() {
+        return "homePage";
+    }
+
     @GetMapping("/home")
     public String getHomePage() {
         return "homePage";
